98f164bddc2ad55c3c7c2e8268600e73b85f3147
Corso Rosati <corsodr@gmail.com>
2024-08-17 17:03:13 -0700

    styling

--- src/app/components/SignIn.tsx
+++ src/app/components/SignIn.tsx
@@ -8,9 +8,14 @@ export function SignIn() {
         await signIn("google", { redirectTo: "/collections" })
       }}
     >
-      <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+       <button
+        type="submit"
+        className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        <span className="mr-2 text-lg font-bold text-blue-500">G</span>
         Continue with Google
       </button>
     </form>
   );
 }
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+// use Next image? 
+// color palette
+// rotate anything 
+
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import { SignIn } from "@/app/components/SignIn";
@@ -11,20 +15,48 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col min-h-screen">
-
-      <header className="container mx-auto px-4 py-4 flex items-center">
-          <h3 className="text-xl font-bold">Shelf</h3>
+      <header className="container mx-auto px-4 py-6">
+        <div className="flex gap-3">
+          <img 
+            src="/images/shelf-logo.svg" alt="Shelf Logo" 
+            className="h-8"
+          />
+          <h3 className="text-2xl font-bold">Shelf</h3>
+        </div>
       </header>
 
-      <main className="flex-grow container mx-auto px-4 py-8 flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold mb-8 text-center">
+      <main className="flex-grow container mx-auto px-4 py-8 flex flex-col items-center">
+        <h1 className="text-4xl font-bold mb-6 text-center">
           Collect anything with Shelf
         </h1>
         <SignIn />
+
+        <div className="mt-12 mb-12">
+          {/* shelf collection image */}
+        </div>
+
+        <section className="w-full max-w-2xl mt-12">
+          <h2 className="text-2xl font-bold mb-4">Vision</h2>
+          <p className="mb-8">
+            Collections are amazing: 
+            {/* images of collections: playlist, library, museum, blog, newsletter, stamps */}
+          </p>
+          <p className="mb-8">
+            Shelf aims to be the best tool for creating and sharing collections online. 
+          </p>
+
+          {/* style this as a list */}
+          <h2 className="text-2xl font-bold mb-4">Roadmap</h2>
+          <ul className="mb-8">
+           <li>First</li>
+           <li>Second</li>
+           <li>Third</li>
+          </ul>
+        </section>
       </main>
 
-      <footer className="bg-gray-100">
-        <div className="container mx-auto px-4 py-4 text-center text-sm text-gray-600">
+      <footer className="bg-gray-100 mt-12">
+        <div className="container mx-auto px-4 py-6 text-center text-sm text-gray-600">
           &copy; 2024 Shelf. All rights reserved.
         </div>
       </footer>
