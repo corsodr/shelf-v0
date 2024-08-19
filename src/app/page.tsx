e72ab32686642ad220fef50b3f13fa9ae3b1c472
Corso Rosati <corsodr@gmail.com>
2024-08-19 11:48:42 -0700

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import { SignIn } from "@/app/components/SignIn";
-import RotatingText from "@/app/components/RotatingText";
 
 export default async function Home() {
   const session = await auth();
@@ -19,34 +18,16 @@ export default async function Home() {
         />
       </header>
 
-      <main className="flex-grow container mx-auto px-4 py-8 flex flex-col items-center">
-        <h1 className="text-6xl font-bold mb-6 text-center leading-tight">
-          Collect <RotatingText />
+      <main className="flex-grow container mx-auto px-4 py-8 flex flex-col justify-start items-center mt-16">
+        <h1 className="text-6xl font-bold mb-10 text-center">
+          Collect anything
           <br />
-          with Shelf
+          with Shelf 
         </h1>
         <SignIn />
-
-        <div className="mt-12 mb-12">
-          {/* shelf collection image */}
-        </div>
-
-        <section className="w-full max-w-2xl mt-12">
-          <h2 className="text-2xl font-bold mb-4">Vision</h2>
-          <p className="mb-8">
-            Show and tell 
-            {/* images of collections: playlist, library, museum, blog, newsletter, stamps */}
-          </p>
-          <h2 className="text-2xl font-bold mb-4">Roadmap</h2>
-          <ul className="list-disc list-inside mb-8">
-           <li>First</li>
-           <li>Second</li>
-           <li>Third</li>
-          </ul>
-        </section>
       </main>
 
-      <footer className="bg-gray-100 mt-12">
+      <footer className="bg-gray-100 mt-auto">
         <div className="container mx-auto px-4 py-6 text-center text-sm text-gray-600">
           &copy; 2024 Shelf. All rights reserved.
         </div>
