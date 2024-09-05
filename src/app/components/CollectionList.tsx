9cbbca22fa81bfba2e9a704d6003f6f83e397975
Corso Rosati <corsodr@gmail.com>
2024-09-05 09:58:28 -0700

    styling

--- src/app/components/CollectionList.tsx
+++ src/app/components/CollectionList.tsx
@@ -5,12 +5,12 @@ export default async function CollectionList() {
   const collections = await getCollections();
 
   return (
-    <nav className="w-80 border-r border-slate-300 overflow-y-auto">
+    <nav className="w-64 border-r border-slate-300 overflow-y-auto">
       <div className="flex flex-col mt-4">
-        <div className="mb-4 px-4">
+      <div className="mb-4 pl-6">
           <Link 
             href="/collections/new" 
-            className="bg-slate-500 hover:bg-slate-700 text-white font-medium py-2 px-4 rounded block text-center"
+            className="bg-slate-500 hover:bg-slate-700 text-white font-medium py-2 px-5 rounded-lg inline-block"
           >
             Create
           </Link>
@@ -20,9 +20,9 @@ export default async function CollectionList() {
             <li key={collection.id}>
               <Link 
                 href={`/collections/${collection.id}`} 
-                className="block py-2 px-2 hover:bg-slate-200 transition-colors duration-150 rounded-lg mx-2"
+                className="block py-2 px-3 hover:bg-slate-200 transition-colors duration-150 rounded-lg mx-3"
               >
-                <h3 className="font-bold">{collection.name}</h3>
+                <h3 className="font-semibold">{collection.name}</h3>
               </Link>
             </li>
           ))}
