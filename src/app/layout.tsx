9d21ff6791099a12a022495cf389efa33109e0aa
Corso Rosati <corsodr@gmail.com>
2024-09-10 09:58:41 -0700

    shelf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Providers } from './providers';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shelf solo",
+  title: "Shelf",
   description: "Collect anything",
 };
 
